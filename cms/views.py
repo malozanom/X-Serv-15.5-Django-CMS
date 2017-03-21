3f886b53fc6ff7c3a7fb9a5ff296b3a1a4da5c33
Your Name <you@example.com>
2017-03-21 23:21:59 +0100

    ejercicio resuelto

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from cms.models import Pages
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def show(request):
+    content = Pages.objects.all()
+    response = ""
+    for entry in content:
+        response = entry.name + " => " + entry.page + "<br>" + response
+    return HttpResponse(response)
+
+
+def entry(request, identifier):
+    entry = Pages.objects.get(id=identifier)
+    response = "La página solicitada es:" + "<br>" + entry.name + \
+               " => " + entry.page
+    return HttpResponse(response)
+
+
+def error(request):
+    response = "La página solicitada no se encuentra en la base de datos"
+    return HttpResponse(response)
